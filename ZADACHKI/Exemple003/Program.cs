04e982d340997bb158cc436403a3bac0db1d991f
Danila <dan_crysis@mail.ru>
2022-06-26 15:44:42 +0300

    решил вторую задачу

--- ZADACHKI/Exemple003/Program.cs
+++ ZADACHKI/Exemple003/Program.cs
@@ -4,7 +4,9 @@
     {
         int[] array = new int[20];
         CreateMassiv_1(array);
-        PrintMassiv_1(array);
+        PrintMassiv_1(array, 625);
+        int n = 25;
+
         void CreateMassiv_1(int[] mass)
         {
             int len = array.Length;
@@ -12,23 +14,29 @@
 
             while (ind < len)
             {
-                mass[ind] =  new Random().Next(1,10);
+                mass[ind] =  new Random().Next(1,50);
                 ind++;
             }
         }
     
-        void PrintMassiv_1(int[] massiv)
+        void PrintMassiv_1(int[] massiv, int n)
         {
             int i = 0;
             int collection = massiv.Length;
 
             while (i < collection)
             {
+                if(Math.Pow(array[i], 2)> n)
+                {
                 Console.Write(massiv[i] + " ");
+                break;
+                }
+                i++;
             }
 
         }
-    
+
+       
     //     int n =25;
     //     int[] array = new int[20];
     //     CreateMassiv(array);
@@ -50,13 +58,6 @@
     //     }
 
        
-    //     // while (index < length)
-    //     // {
-    //     //     if (Math.Pow(array[index], 2)< n)
-    //     //     {
-    //     //         Console.Write(array[index] + " ");
-    //     //     }
-    //     //     index++;
-    //     // }
+   
     }
 }
